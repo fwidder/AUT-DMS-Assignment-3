320d46f9d7d92c1d392448932edcc5acf3d45e3c
Florian Widder <florian.widder@live.de>
2018-05-10 00:54:30 +1200

    Add Users & Reorganize packages

--- app/src/main/java/com/hotservice/sauron/utils/PhoneNumberChecker.java
+++ app/src/main/java/com/hotservice/sauron/utils/PhoneNumberChecker.java
@@ -9,6 +9,6 @@ public class PhoneNumberChecker extends AsyncTask<AppCompatActivity, Void, Strin
     protected String doInBackground(AppCompatActivity... appCompatActivities) {
         //TelephonyManager tMgr = (TelephonyManager) AppContext.getSystemService(Context.TELEPHONY_SERVICE);
         //String mPhoneNumber = tMgr.getLine1Number();
-        return mPhoneNumber;
+        return "";
     }
 }
