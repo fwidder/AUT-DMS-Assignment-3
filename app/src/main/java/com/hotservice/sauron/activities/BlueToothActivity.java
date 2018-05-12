aa701a89287b92e36c0282f171a9aa1a7cee57f6
wjm2202 <wjm2202@aut.ac.nz>
2018-05-12 13:28:02 +1200

    start a background service prototype

---
+++ app/src/main/java/com/hotservice/sauron/activities/BlueToothActivity.java
@@ -0,0 +1,15 @@
+package com.hotservice.sauron.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hotservice.sauron.R;
+
+public class BlueToothActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blue_tooth);
+    }
+}
