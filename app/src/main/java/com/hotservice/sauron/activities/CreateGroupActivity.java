7aafa52862905169ca0dc810b96a59e2e464768d
wjm2202 <wjm2202@aut.ac.nz>
2018-05-12 12:09:05 +1200

    create qr code for create group remove floating tab input is string for qr code

--- app/src/main/java/com/hotservice/sauron/activities/CreateGroupActivity.java
+++ app/src/main/java/com/hotservice/sauron/activities/CreateGroupActivity.java
@@ -10,17 +10,20 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.hotservice.sauron.R;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 public class CreateGroupActivity extends AppCompatActivity {
 
     private Button button;
     private EditText editText;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +33,8 @@ public class CreateGroupActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         final Context context  = this;
-        editText = (EditText) this.findViewById(R.id.editText);
+        editText = (EditText) this.findViewById(R.id.nameBox);
+        imageView = (ImageView) this.findViewById(R.id.createGroupQR);
         button = (Button) this.findViewById(R.id.saveButton);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,8 +43,9 @@ public class CreateGroupActivity extends AppCompatActivity {
                 MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
                 try {
                     BitMatrix bitMatrix = multiFormatWriter.encode(text2QR, BarcodeFormat.QR_CODE,200,200);
-                    //BarcodeEncoder
-                    //Bitmap bitmap = barcodeEncoder
+                    BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+                    Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+                    imageView.setImageBitmap(bitmap);
                 } catch (WriterException e) {
                     e.printStackTrace();
                 }
